9746dda84c535c7e17f5899e91d32bd9a0db3d4a
Valera <valerydec17@gmail.com>
2018-06-10 02:17:20 +0700

    altspider: ideally crawls the site

--- Learn/Scrapper-2/tutsplus/tutsplus/spiders/myaltspider.py
+++ Learn/Scrapper-2/tutsplus/tutsplus/spiders/myaltspider.py
@@ -3,33 +3,33 @@ from tutsplus.items import TutsplusItem
 from scrapy.http    import Request
 import re
 import os
-# import pdb
 
-
+crawledLinks = []
 
 class MySpider(Spider):
 	name            = "myalt"
 	allowed_domains = ["alternativeto.net"]
 	start_urls      = ["https://alternativeto.net/"]
 
+
 	def parse(self, response):
+		os.system('echo ' + ' !!!!!!!!!!!!! ' + ' >> alt_log_1.txt')
+		os.system('echo ' + ' '.join(crawledLinks) + ' >> alt_log_1.txt')
 		links = response.xpath('//a/@href').extract()
 		
-		crawledLinks = []
-		linkPattern = re.compile("^\/software\/")
+		
+		linkPattern = re.compile("^\/software\/[a-zA-Z0-9\-]+\/$")
 
 		linksToCrawl = []
 		for link in links:
 			if linkPattern.match(link) and not link in crawledLinks:
 				linksToCrawl.append(link)
-
-		# pdb.set_trace()
-		# print linksToCrawl
+				crawledLinks.append(link)
 
 		for link in linksToCrawl:
 			link = "https://alternativeto.net" + link
 			print "New link started: ", link, "  CrawledPages: ", str(len(crawledLinks)) 
 			os.system('echo ' + 'Link num. : ' + str(len(crawledLinks)) + ' Link :  ' + link + ' >> alt_log.txt')
-			crawledLinks.append(link)
+			# crawledLinks.append(link)
 			yield Request(link, self.parse)
 
